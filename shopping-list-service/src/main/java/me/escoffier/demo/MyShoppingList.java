080c26613666eb3ec8e306394e27c7e48f5ac14d
Clement Escoffier <clement.escoffier@gmail.com>
2017-04-25 14:51:42 -0300

    Preparation commit

---
+++ shopping-list-service/src/main/java/me/escoffier/demo/MyShoppingList.java
@@ -0,0 +1,80 @@
+package me.escoffier.demo;
+
+import io.vertx.circuitbreaker.CircuitBreakerOptions;
+import io.vertx.core.json.JsonObject;
+import io.vertx.rxjava.circuitbreaker.CircuitBreaker;
+import io.vertx.rxjava.core.AbstractVerticle;
+import io.vertx.rxjava.core.http.HttpServerResponse;
+import io.vertx.rxjava.ext.web.Router;
+import io.vertx.rxjava.ext.web.RoutingContext;
+import io.vertx.rxjava.ext.web.client.HttpResponse;
+import io.vertx.rxjava.ext.web.client.WebClient;
+import io.vertx.rxjava.servicediscovery.ServiceDiscovery;
+import io.vertx.rxjava.servicediscovery.types.HttpEndpoint;
+import rx.Observable;
+import rx.Single;
+
+/**
+ * @author clement
+ */
+public class MyShoppingList extends AbstractVerticle {
+
+  WebClient shopping, pricer;
+
+  @Override
+  public void start() {
+
+    Router router = Router.router(vertx);
+    router.route("/health").handler(rc -> rc.response().end("OK"));
+    router.route("/").handler(this::getShoppingList);
+
+    ServiceDiscovery.create(vertx, discovery -> {
+      // Get pricer-service
+
+      // Get shopping-backend
+
+    });
+
+    vertx.createHttpServer()
+      .requestHandler(router::accept)
+      .listen(8080);
+  }
+
+  private void getShoppingList(RoutingContext rc) {
+    HttpServerResponse serverResponse =
+      rc.response().setChunked(true);
+
+        /*
+         +--> Retrieve shopping list
+           +
+           +-->  for each item, call the pricer, concurrently
+                    +
+                    |
+                    +-->  For each completed evaluation (line),
+                          write it to the HTTP response
+         */
+
+
+        /*
+                               shopping          pricer
+                               backend
+                 +                +                +
+                 |                |                |
+                 +--------------> |                |
+                 |                |                |
+                 |                |                |
+                 +-------------------------------> |
+                 |                |                |
+                 +-------------------------------> |
+        write <--|                |                |
+                 +-------------------------------> |
+        write <--|                +                +
+                 |
+        write <--|
+                 |
+          end <--|
+         */
+
+  }
+
+}
